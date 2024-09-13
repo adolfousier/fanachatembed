43a9f72079a38ad62d6bbb5d72d96aa37fefbb16
Adolfo Usier <adolfousier@gmail.com>
2024-09-14 00:07:00 +0100

    patching textarea

--- src/components/inputs/textInput/components/ShortTextInput.tsx
+++ src/components/inputs/textInput/components/ShortTextInput.tsx
@@ -43,7 +43,6 @@ export const ShortTextInput = (props: ShortTextInputProps) => {
     <textarea
       ref={props.ref}
       class="focus:outline-none bg-transparent px-4 py-4 flex-1 w-full h-full min-h-[56px] max-h-[128px] text-input"
-      disabled={props.disabled !== undefined ? props.disabled : false}
       style={{
         'font-size': props.fontSize ? `${props.fontSize}px` : '16px',
         resize: 'none',
